ba2cf44885840734af11e4feaf3e192f56ba563d
farhanswitch <muhammad_farhan@outlook.co.id>
2022-08-06 08:09:35 -0700

    basic setup tailwindcss and react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./styles/style.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);

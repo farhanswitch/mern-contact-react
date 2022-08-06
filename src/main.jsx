1eb1057c637e7fd447f0d029b1e2ce0506568842
farhanswitch <muhammad_farhan@outlook.co.id>
2022-08-06 10:28:16 -0700

    add image to register and login page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./styles/style.css";
 import App from "./App";
 import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,42 +3,26 @@ import { Link } from "react-router-dom";
 
 import Layout from "../components/Layout";
 
-const RegisterPage = () => {
-  const [name, setName] = useState("");
+const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [password1, setPassword1] = useState("");
 
   return (
-    <Layout pageTitle="Register">
-      <div className="register w-full mx-auto flex justify-center items-center ">
+    <Layout pageTitle="Login">
+      <div className="login w-full mx-auto flex justify-center items-center ">
         <div className="inner min-w-[600px] flex items-stretch border border-slate-200 rounded-xl shadow-xl text-slate-700">
-          <div className="left bg-sky-500/70 w-[200px]  min-h-full grid place-items-center ">
+          <div className="left bg-green-500/70 w-[200px]  min-h-full grid place-items-center  ">
             <div className="bg-white/80 shadow-lg shadow-white/30 backdrop-blur-md border p-4 rounded-lg ">
               <img
-                src="/woman2.png"
+                src="/man2.png"
                 alt="man"
                 className="w-[100px] rounded-full"
               />
             </div>
           </div>
           <div className="right flex-1 h-full py-4">
-            <h2 className="text-center text-blue-600 mb-4">
-              Register an Account
-            </h2>
+            <h2 className="text-center text-blue-600 mb-4">Login</h2>
             <form className="py-5 w-full block max-w-3xl mx-auto px-6">
-              <div className="form-element mt-3">
-                <label htmlFor="name">Full Name</label>
-                <input
-                  className="px-2 py-1 border block w-full mt-1 rounded"
-                  type="text"
-                  name="name"
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-              </div>
               <div className="form-element mt-3">
                 <label htmlFor="email">Email Address</label>
                 <input
@@ -63,28 +47,17 @@ const RegisterPage = () => {
                   required
                 />
               </div>
-              <div className="form-element mt-3">
-                <label htmlFor="c-password">Confirm Password</label>
-                <input
-                  className="px-2 py-1 border block w-full mt-1 rounded"
-                  type="password"
-                  name="c-password"
-                  id="c-password"
-                  value={password1}
-                  onChange={(e) => setPassword1(e.target.value)}
-                  required
-                />
-              </div>
+
               <button
                 className="bg-blue-600 hover:bg-sky-600 text-white text-sm rounded mt-8 px-2 py-1"
                 type="submit"
               >
-                Sign Up
+                Sign In
               </button>
               <p className="text-sm mt-4 text-slate-500">
-                Already have an account ?
-                <Link className="text-blue-700 ml-2" to={"/login"}>
-                  Login
+                Do not have an account yet ?
+                <Link className="text-blue-700 ml-2" to={"/register"}>
+                  Register
                 </Link>
               </p>
             </form>
@@ -95,4 +68,4 @@ const RegisterPage = () => {
   );
 };
 
-export default RegisterPage;
+export default LoginPage;
